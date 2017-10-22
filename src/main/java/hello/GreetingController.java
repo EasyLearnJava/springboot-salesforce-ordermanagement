e7efb717b2bd9e4a232bbb2581d683848842c391
Easy Learn Java <easylearnjava4u@gmail.com>
2017-10-21 22:18:44 -0500

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
